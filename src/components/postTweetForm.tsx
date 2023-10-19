12f7d1e9aa012194e2ecf37c821726a7861223ee
yoooooooung <oaoa9n@gmail.com>
2023-10-19 15:55:49 +0900

    3.3 Uploading Images

--- src/components/postTweetForm.tsx
+++ src/components/postTweetForm.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { auth, db } from "../firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { auth, db, storage } from "../firebase";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function PostTweetForm() {
   const [isLoading, setIsLoading] = useState(false);
@@ -37,12 +38,35 @@ export default function PostTweetForm() {
        * 새로운 documnet를 생성
        * addDoc(collection(Friestore 인스턴스(=db), 'collectoin이름'), {추가하고 싶은 데이터})
        */
-      await addDoc(collection(db, "tweet"), {
+      const doc = await addDoc(collection(db, "tweet"), {
         tweet, // tweet : tweet 이랑 같은 뜻인듯
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid,
       });
+      // 만약 파일까지 첨부했다면
+      if (file) {
+        // 해당 파일 참조 위치를 받아야함
+        // ref(storage인스턴스, 파일이 저장될 URL)
+        // tweets라는 폴더내에 각자의 userid 안에 트윗id 안에 저장하기
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        // uploadByte(어디에 저장할지, 저장할 파일)
+        // uploadByte는 promise를 반환.
+        // 반환되는 값(업로드 결과 참조값)을 저장해서
+        const result = await uploadBytes(locationRef, file);
+        // 파라미터로 받은 데이터를 URL로 반환
+        const url = await getDownloadURL(result.ref);
+        // 데이터(document) 업데이트함수(어떤 데이터, 추가할 데이터)
+        await updateDoc(doc, {
+          photo: url,
+        });
+      }
+      // DB에 저장 후 textarea 리셋
+      setTweet("");
+      setFile(null);
     } catch (e) {
       console.log(e);
     } finally {
@@ -53,6 +77,7 @@ export default function PostTweetForm() {
   return (
     <From onSubmit={onSubmit}>
       <TextArea
+        required
         rows={5}
         maxLength={180}
         placeholder="What is Happening?"
